7c3f783e9e20908327e9286ce0a0c9b190966620
srujansuvarna <159527378+srujansuvarna@users.noreply.github.com>
2024-02-13 20:09:42 -0800

    Add files via upload

---
+++ add.java
@@ -0,0 +1,16 @@
+
+import java.util.Scanner;
+class add
+{
+ public static void main(String args[])
+ {
+  int a,b,sum;
+  Scanner s=new Scanner(System.in);
+  System.out.println("enter the first number");
+  a=s.next.int();
+  System.out.println("enter the second number");
+  b=s.next.int();
+  sum=a+b
+ System.out.println("sum of two number"=+sum)
+ }
+}
